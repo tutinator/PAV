ebdd259e43774b82634d9e86395d827b0bb2856d
tutinator <tutinatorcba@gmail.com>
2014-09-18 17:57:46 -0300

    ABM Clubes listo

--- Asociacion (TP)/Asociacion/Asociacion/Formularios/frm_ABMClubes.vb
+++ Asociacion (TP)/Asociacion/Asociacion/Formularios/frm_ABMClubes.vb
@@ -29,7 +29,10 @@ Public Class frm_ABMclubes
         Me.cmd_nuevo.Enabled = True
         Me.cmd_buscar.Enabled = True
         Me.cmd_salir.Enabled = True
+        Me.txt_codClub.Enabled = True
+        Me.txt_nombre.Enabled = True
         Me.txt_codClub.Focus()
+
     End Sub
 
     Private Sub limpiarCampos()
@@ -72,9 +75,9 @@ Public Class frm_ABMclubes
 
     Private Sub cambiarEntradas(ByVal x As Boolean)
         limpiarCampos()
-        'txt_codClub.Enabled = x
+        txt_codClub.Enabled = x
         txt_calle.Enabled = x
-        'txt_nombre.Enabled = x
+        txt_nombre.Enabled = x
         txt_nroCalle.Enabled = x
         cmb_codPos.Enabled = x
         txt_telefono.Enabled = x
@@ -100,7 +103,7 @@ Public Class frm_ABMclubes
 
         Dim consulta As String = ""
         consulta = "SELECT Clubes.codClub AS Codigo, Clubes.nombre AS Nombre, Clubes.calle AS Calle, Clubes.numero AS Numero, Clubes.telefono AS Telefono, "
-        consulta &= "CodPostales.nombre AS [Codigo Postal] "
+        consulta &= "CodPostales.codPos AS [Codigo Postal], CodPostales.nombre AS [Area] "
         consulta &= "FROM Clubes INNER JOIN CodPostales ON Clubes.codPos = CodPostales.codPos"
 
         grid_clubes.DataSource = acceso.ejecutar(consulta)
@@ -112,6 +115,7 @@ Public Class frm_ABMclubes
         combo.DataSource = nombre_tabla
         combo.ValueMember = pk
         combo.DisplayMember = descrip
+        combo.SelectedIndex = -1
 
     End Sub
 
@@ -126,7 +130,7 @@ Public Class frm_ABMclubes
         consulta &= "calle = '" & Me.txt_calle.Text & "', "
         consulta &= "numero = " & Me.txt_nroCalle.Text & ", "
         consulta &= "telefono = " & Me.txt_telefono.Text & ", "
-        consulta &= "telefono = " & Me.txt_telefono.Text & " "
+        consulta &= "codPos = " & Me.cmb_codPos.SelectedValue.ToString & " "
         consulta &= "WHERE codClub = " & Me.txt_codClub.Text
 
         acceso.ejecutarNonConsulta(consulta)
@@ -141,7 +145,7 @@ Public Class frm_ABMclubes
         consulta = "INSERT into Clubes "
         consulta &= "values ('" & Me.txt_codClub.Text & "', '" & Me.txt_nombre.Text & "', "
         consulta &= "'" & Me.txt_calle.Text & "', " & Me.txt_nroCalle.Text & ", "
-        consulta &= Me.txt_telefono.Text & ", " & Me.cmb_codPos.SelectedValue.ToString & ")"
+        consulta &= Me.txt_telefono.Text & ", " & Me.cmb_codPos.SelectedValue & ")"
 
         acceso.ejecutarNonConsulta(consulta)
         Return termino.aprobado
@@ -163,6 +167,13 @@ Public Class frm_ABMclubes
             Return termino.aprobado
         End If
     End Function
+
+    Private Function delete() As termino
+        Dim consulta As String = "DELETE FROM Clubes WHERE codClub = " & Me.txt_codClub.Text
+        acceso.ejecutarNonConsulta(consulta)
+        Return termino.aprobado
+    End Function
+
     'Comandos
 
     Private Sub cmd_guardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_guardar.Click
@@ -193,14 +204,14 @@ Public Class frm_ABMclubes
     Private Sub cmd_eliminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_eliminar.Click
         If txt_codClub.Text = "" Then
             MsgBox("No se ha seleccionado ningún club", MsgBoxStyle.Critical, "Error")
+        Else
+            If MessageBox.Show("¿Está seguro que desea eliminar el Club " & txt_nombre.Text & "?", "Atención", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1) = Windows.Forms.DialogResult.OK Then
+                If Me.delete() = termino.aprobado Then
+                    MessageBox.Show("Club eliminado", "Operación completada")
+                    Me.inicio()
+                End If
+            End If
         End If
-
-        If MessageBox.Show("¿Está seguro que desea eliminar el Club " & txt_codClub.Text & "?", "Atención", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1) = Windows.Forms.DialogResult.OK Then
-            'insertar codigo para borrar club de la base de datos
-            MessageBox.Show("Club eliminado", "Operación completada")
-            Me.inicio()
-        End If
-
     End Sub
 
     Private Sub cmd_salir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_salir.Click
@@ -214,47 +225,82 @@ Public Class frm_ABMclubes
         Me.cmd_cancelar.Enabled = True
         Me.cmd_guardar.Enabled = True
         txt_codClub.Focus()
+        Me.accion = estado.insertar
     End Sub
 
     Private Sub cmd_buscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_buscar.Click
         cambiarBotones(False)
-        'cambiarEntradas(False)
         Me.cmd_cancelar.Enabled = True
+        Me.cmd_buscar.Enabled = True
 
         If txt_nombre.Text <> "" And txt_codClub.Text <> "" Then
             MsgBox("Ingrese sólo un criterio de búsqueda", MsgBoxStyle.Critical, "Importante")
             txt_codClub.Focus()
-            cmd_buscar.Enabled = True
-        End If
 
-        If txt_codClub.Text = "" Then
-            If txt_nombre.Text = "" Then
-                MsgBox("Ingrese un dato para buscar", MsgBoxStyle.Critical, "Importante")
-                txt_codClub.Focus()
-                cmd_buscar.Enabled = True
+        Else
+
+            If txt_codClub.Text = "" Then
+                If txt_nombre.Text = "" Then
+                    MsgBox("Ingrese un dato para buscar", MsgBoxStyle.Critical, "Importante")
+                    txt_codClub.Focus()
+
+                Else
+                    Dim consulta As String = ""
+                    Dim dt As New Data.DataTable
+                    consulta = "SELECT * FROM Clubes WHERE nombre LIKE '%" & Me.txt_nombre.Text & "%'"
+                    dt = acceso.ejecutar(consulta)
+
+                    grid_clubes.DataSource = dt
+                End If
             Else
-                'busqueda por nombre
-
-                cmd_buscar.Enabled = True
+                Dim consulta As String = ""
+                Dim dt As New Data.DataTable
+                consulta = "SELECT * FROM Clubes WHERE codClub = '" & Me.txt_codClub.Text & "'"
+                dt = acceso.ejecutar(consulta)
+                grid_clubes.DataSource = dt
 
             End If
-        Else
-            'busqueda por codigo
-            cmd_buscar.Enabled = True
-
         End If
 
     End Sub
 
-
-    Private Sub cmd_nuevoCP_Click(sender As Object, e As EventArgs) Handles cmd_nuevoCP.Click
+    Private Sub cmd_nuevoCP_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmd_nuevoCP.Click
         frm_ABMCodPos.ShowDialog()
 
         'Agregar funcionalidad para que al crearse el cp nuevo, se muestre en el cbo de cp
 
     End Sub
 
+    Private Sub grid_clubes_CellMouseDoubleClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles grid_clubes.CellMouseDoubleClick
 
+        Dim codigoSeleccionado As String = Me.grid_clubes.CurrentRow.Cells(0).Value
+
+        Dim tabla As New Data.DataTable
+
+        Dim consulta As String = ""
+        consulta = "SELECT * FROM Clubes WHERE codClub = " & codigoSeleccionado
+
+        cambiarEntradas(True)
+        tabla = acceso.ejecutar(consulta)
+
+        Me.txt_codClub.Text = tabla.Rows(0)("codClub")
+        Me.txt_nombre.Text = tabla.Rows(0)("nombre")
+        Me.txt_calle.Text = tabla.Rows(0)("calle")
+        Me.txt_nroCalle.Text = tabla.Rows(0)("numero")
+        Me.txt_telefono.Text = tabla.Rows(0)("telefono")
+        Me.cmb_codPos.SelectedValue = tabla.Rows(0)("codPos")
+
+        cambiarBotones(False)
+        Me.cmd_cancelar.Enabled = True
+        Me.cmd_eliminar.Enabled = True
+        Me.cmd_guardar.Enabled = True
+
+        Me.txt_codClub.Enabled = False
+        txt_nombre.Focus()
+
+        Me.accion = estado.modificar
+
+    End Sub
 
 
     'Closing
@@ -268,14 +314,8 @@ Public Class frm_ABMclubes
 
 
 
-    Private Sub cmb_codPos_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmb_codPos.SelectedIndexChanged
-        ' Dim consulta As String = ""
-        'consulta = "SELECT nombre FROM CodPostales WHERE codPos = " & cmb_codPos.SelectedValue.ToString
-        'Dim tabla As Data.DataTable
-
-        'tabla = acceso.ejecutar(consulta)
-
-        '        lbl_codPos.Text = tabla.Rows(0)("nombre").ToString
-
+    
+    Private Sub cmb_codPos_DropDown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmb_codPos.DropDown
+        cargar_combo(cmb_codPos, leo_tabla("CodPostales"), "codPos", "codPos")
     End Sub
 End Class
