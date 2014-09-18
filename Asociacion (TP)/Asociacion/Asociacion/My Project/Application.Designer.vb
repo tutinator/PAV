f2e7083a33ee793e34ec48d52638a4013f861817
tutinator <tutinatorcba@gmail.com>
2014-09-18 16:17:59 -0300

    Por empezar con trumba

--- Asociacion (TP)/Asociacion/Asociacion/My Project/Application.Designer.vb
+++ Asociacion (TP)/Asociacion/Asociacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Asociacion.frm_ABMTorneo
+            Me.MainForm = Global.Asociacion.frm_Menu
         End Sub
     End Class
 End Namespace
